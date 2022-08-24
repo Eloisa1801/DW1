ad449e103ad666cec362839ce223b6b1a92189df
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2022-08-23 20:05:07 -0400

    aula python

---
+++ app.py
@@ -0,0 +1,16 @@
+from crypt import methods
+from flask import Flask, render_template #importando flask
+from flask import request
+meu_app = Flask(__name__)
+@meu_app.route('/') #definindo rota
+@meu_app.route('/salvar', methods=['POST'])
+def to_index(): #funcao python
+    lista=['Eloisa', 'Leticia', 'Paulo']
+    return render_template('index.html', lista=lista)
+#FLASK_APP=app.py
+#flask run
+#FLASK_ENV=development
+#flask run
+#export FLASK_ENV=development - atualizar 
+#{{}} html entende que é a variavel do phyton
+#flask run --host=0.0.0.0
